abee5ff19728ecd6e24c924b7be00cd32f4834cf
Georgin Saju <sajugeorgin@gmail.com>
2025-11-13 22:47:27 +0000

    build initial banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
+import banner from "@/assets/banner.jpg";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <Button>INCREASE</Button>
-    </div>
+    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
+      {/* BANNER */}
+      <div className="bg-secondary flex items-center rounded-xl md:h-96">
+        {/* LEFT SIDE */}
+        <div className="space-y-7 p-10 text-center md:w-1/2">
+          <h1 className="text-3xl font-bold md:text-4xl">
+            Simple pieces, strong looks.
+          </h1>
+
+          <p>
+            Quality materials, careful construction, and easy styling. Dress
+            them up or down and feel confident every day.
+          </p>
+
+          <Button
+            asChild
+            className="cursor-pointer transition duration-300 hover:scale-105"
+          >
+            <Link href="/shop">
+              Shop Now <ArrowRight className="size-5" />
+            </Link>
+          </Button>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="relative hidden h-full w-1/2 md:block">
+          <Image src={banner} alt="banner" className="h-full object-cover" />
+          <div className="from-secondary absolute inset-0 bg-linear-to-r via-transparent to-transparent" />
+        </div>
+      </div>
+    </main>
   );
 }
